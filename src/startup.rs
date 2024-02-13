e501b9f9d97ba6c1ef92a6491755f3daa3ac4d5e
Yotam Kanny <yotamkanny3@gmail.com>
2024-02-12 22:43:29 -0700

    Fix more clippy errors

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::{PgPool};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
